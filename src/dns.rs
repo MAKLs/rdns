1093b44a659860e04a134ffc6935733b24a24bb7
MAKLs <mikeashimko@gmail.com>
2020-03-02 21:25:58 -0500

    Add QueryType enum

--- src/dns.rs
+++ src/dns.rs
@@ -25,6 +25,28 @@ impl ResponseCode {
     }
 }
 
+#[derive(PartialEq, Eq, Debug, Clone, Hash, Copy)]
+pub enum QueryType {
+    UNKNOWN(u16),
+    A,
+}
+
+impl QueryType {
+    pub fn to_num(&self) -> u16 {
+        match *self {
+            QueryType::UNKNOWN(n) => n,
+            QueryType::A => 1,
+        }
+    }
+
+    pub fn from_num(num: u16) -> QueryType {
+        match num {
+            1 => QueryType::A,
+            _ => QueryType::UNKNOWN(num),
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct DnsHeader {
     pub id: u16,
