acf856c555cbce8087c526233a77cde3874d1dbb
MAKLs <mikeashimko@gmail.com>
2020-03-03 20:06:26 -0500

    Add DnsRecord implementation

--- src/dns.rs
+++ src/dns.rs
@@ -1,3 +1,4 @@
+use std::net::Ipv4Addr;
 use std::io::{Error, ErrorKind, Read, Result};
 use crate::packet::*;
 
@@ -142,5 +143,54 @@ impl DnsQuestion {
         let _ = buffer.read_u16()?;
         
         Ok(())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[allow(dead_code)]
+pub enum DnsRecord {
+    UNKNOWN {
+        domain: String,
+        qtype: u16,
+        data_len: u16,
+        ttl: u32
+    },
+    A {
+        domain: String,
+        addr: Ipv4Addr,
+        ttl: u32
+    }
+}
+
+impl DnsRecord {
+
+    pub fn read(buffer: &mut BytePacketBuffer) -> Result<DnsRecord> {
+        let mut domain = String::new();
+        buffer.read_qname(&mut domain)?;
+
+        let qtype = buffer.read_u16()?;
+        let _ = buffer.read_u16()?;     // ignore class
+        let ttl = buffer.read_u32()?;
+        let data_len = buffer.read_u16()?;
+
+        match QueryType::from_num(qtype) {
+            QueryType::A => {
+                let raw_addr = buffer.read_u32()?;
+                // TODO: refactor
+                let addr = Ipv4Addr::new(
+                    ((raw_addr >> 24) & 0xFF) as u8,
+                    ((raw_addr >> 16) & 0xFF) as u8,
+                    ((raw_addr >> 8) & 0xFF) as u8,
+                    ((raw_addr >> 0) & 0xFF) as u8
+                );
+
+                Ok(DnsRecord::A {domain, addr, ttl})
+            },
+            QueryType::UNKNOWN(_) => {
+                buffer.step(data_len as usize)?;
+
+                Ok(DnsRecord::UNKNOWN {domain, ttl, data_len, qtype })
+            }
+        }
     }
 }
