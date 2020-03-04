1b89eebbbc93981850987c4d77b309bf89d3628b
MAKLs <mikeashimko@gmail.com>
2020-03-03 20:21:22 -0500

    Combine DNS structs into packet struct

--- src/dns.rs
+++ src/dns.rs
@@ -194,3 +194,60 @@ impl DnsRecord {
         }
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct DnsPacket {
+    pub header: DnsHeader,
+    pub questions: Vec<DnsQuestion>,
+    pub answers: Vec<DnsRecord>,
+    pub authorities: Vec<DnsRecord>,
+    pub resources: Vec<DnsRecord>
+}
+
+impl DnsPacket {
+    pub fn new() -> DnsPacket {
+        DnsPacket {
+            header: DnsHeader::new(),
+            questions: Vec::new(),
+            answers: Vec::new(),
+            authorities: Vec::new(),
+            resources: Vec::new(),
+        }
+    }
+
+    pub fn from_buffer(buffer: &mut BytePacketBuffer) -> Result<DnsPacket> {
+        // Read in header
+        let mut result = DnsPacket::new();
+        result.header.read(buffer)?;
+
+        // TODO: refactor reading records from buffer
+
+        // Read in questions
+        for _ in 0 .. result.header.questions {
+            let mut question = DnsQuestion::new(String::from(""), QueryType::UNKNOWN(0));
+
+            question.read(buffer)?;
+            result.questions.push(question);
+        }
+
+        // Read in answers
+        for _ in 0 .. result.header.answers {
+            let answer = DnsRecord::read(buffer)?;
+            result.answers.push(answer);
+        }
+
+        // Read in authorities
+        for _ in 0 .. result.header.authoritative_entries {
+            let authority = DnsRecord::read(buffer)?;
+            result.authorities.push(authority);
+        }
+
+        // Read in resources
+        for _ in 0 .. result.header.resource_entries {
+            let resource = DnsRecord::read(buffer)?;
+            result.resources.push(resource);
+        }
+
+        Ok(result)
+    }
+}
