aa8a6419dc2e71e14c36a67ef61f894eefde8285
MAKLs <mikeashimko@gmail.com>
2020-04-05 18:55:37 -0400

    Implement sending DNS packet downstream via TCP

--- src/dns/network.rs
+++ src/dns/network.rs
@@ -1,7 +1,7 @@
-use super::buffer::{BytePacketBuffer, ByteBuffer};
+use super::buffer::{ByteBuffer, BytePacketBuffer, ExtendingBuffer, VariableBuffer};
 use super::protocol::{DnsPacket, DnsQuestion, QueryType};
-use std::io::{Error, ErrorKind, Result};
-use std::net::UdpSocket;
+use std::io::{Error, ErrorKind, Read, Result, Write};
+use std::net::{TcpStream, UdpSocket};
 use std::sync::atomic::{AtomicU16, Ordering};
 
 pub struct NetworkClient {
@@ -24,8 +24,35 @@ impl NetworkClient {
         server: (&str, u16),
         recursive: bool,
     ) -> Result<DnsPacket> {
-        println!("Would query {} over TCP", qname);
-        unimplemented!();
+        // Set up connection to downstream server
+        let mut stream = TcpStream::connect(&server)?;
+
+        // Prepare question packet to send downstream
+        let mut packet = DnsPacket::new();
+        packet.header.id = self.pid_seq.fetch_add(1, Ordering::SeqCst);
+        packet.header.questions = 1;
+        packet.header.recursion_desired = recursive;
+        packet
+            .questions
+            .push(DnsQuestion::new(String::from(qname), qtype));
+
+        // Write question into buffer and send request
+        let mut req_buffer = BytePacketBuffer::new();
+        let data_len = packet.write(&mut req_buffer).unwrap();
+        let mut len_buffer = [0; 2];
+        len_buffer[0] = (data_len >> 8) as u8;
+        len_buffer[1] = (data_len & 0xFF) as u8;
+        stream.write(&len_buffer)?;
+        stream.write(&req_buffer.buf[0..req_buffer.head()])?;
+
+        // Read the response
+        let mut len_buffer = [0; 2];
+        stream.read(&mut len_buffer)?;
+        let buf_len = ((len_buffer[0] as u16) << 8) | (len_buffer[1] as u16);
+        let mut res_buffer = VariableBuffer::new(buf_len as usize);
+        stream.read(&mut res_buffer.buf).unwrap();
+
+        DnsPacket::from_buffer(&mut res_buffer)
     }
 
     fn send_udp_query(
